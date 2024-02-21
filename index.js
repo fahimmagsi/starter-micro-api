b7c99fdcf286e26adffb2e03a0339c1bbdb175d2
fahimmagsi <fahimmagsi@gmail.com>
2024-02-21 22:46:52 +0500

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const {Client} = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const {Configuration, OpenAIApi} = require("openai");
+require("dotenv").config();
+
+const client = new Client();
+
+client.on('qr',(qr) =>{
+    qrcode.generate(qr,{small:true});
+});
+
+client.on('ready',() =>{
+    console.log("Client is ready");
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+    apiKey : process.env.SECRET_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(message){
+    const completion = await openai.createCompletion({
+        model:"text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
+
+client.on('message',message => {
+    console.log(message.body);
+    runCompletion(message.body).then(result => message.reply(result));
+})
